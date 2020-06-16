0622867e134d6de127ab9ac1335e45b20d59d2bf
redsight <malahov.dmitry@gmail.com>
2020-06-16 14:27:09 +0400

    Добавлены файлы

---
+++ src/components/Main.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="temp-main">
+    <router-link to="Game" class="btn btn_link">Открыть</router-link>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+.temp-main
+    width: 100vw
+    height: 100vh
+    display: flex
+    align-items: center
+</style>
